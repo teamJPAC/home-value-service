72318219948bca8a3ceecdee9c7537c61fe74d11
Jackie Chen <chen.jackie.k@gmail.com>
2018-11-13 21:06:33 -0600

    changed back to localhost
    got server working

--- server/database/index.js
+++ server/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri = 'mongodb+srv://admin1:admin1password@cluster0-ytvdt.mongodb.net/houses?retryWrites=true/';
+const uri = 'mongodb://localhost/houses';
 
 const db = mongoose.connect(
   uri,
