94be03ccba007b42b7a50ac06fef9b8daeae3b08
Jackie Chen <chen.jackie.k@gmail.com>
2018-11-14 01:17:30 -0600

    create schema for server

---
+++ server/database/cassandraPopulate.js
@@ -0,0 +1,42 @@
+var ExpressCassandra = require('express-cassandra');
+
+var models = ExpressCassandra.createClient({
+  clientOptions: {
+    contactPoints: ['127.0.0.1'],
+    protocolOptions: { port: 9042 },
+    keyspace: 'mykeyspace',
+    queryOptions: { consistency: ExpressCassandra.consistencies.one }
+  },
+  ormOptions: {
+    defaultReplicationStrategy: {
+      class: 'SimpleStrategy',
+      replication_factor: 1
+    },
+    migration: 'safe',
+  }
+});
+
+var MyModel = models.loadSchema('Home', {
+  fields: {
+    _id: "text",
+    address: "text",
+    city: "text",
+    zip: "int",
+    zestimate: { type: "set", typeDef: "<varint>"},
+    beds: "int",
+    baths: "int",
+    sqFt: "int",
+    status: "text",
+    taxAssessment: "int",
+  },
+  key: ["_id"]
+});
+
+console.log(models.modelInstance.Home === MyModel);
+
+MyModel.syncDB((err, result) => {
+  if(err){
+    throw err;
+  }
+  console.log('schemed was altered', result);
+})
