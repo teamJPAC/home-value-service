73ab3cd94b8e465ed84753b4608ce281ae37d296
Jackie Chen <chen.jackie.k@gmail.com>
2018-11-13 21:07:30 -0600

    modified localhost for get request

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { PreQuery } from './components/PreQuery.js';
 const port = 8081;
 
 const client = new ApolloClient({
-  uri: 'http://hr-fec-otb.us-west-1.elasticbeanstalk.com/graphql',
+  uri: `http://localhost:${port}/graphql`,
 });
 
 ReactDOM.render(
