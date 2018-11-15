cd5ee41bd59276cdb8c1886885cd68d85f027c67
Jackie Chen <chen.jackie.k@gmail.com>
2018-11-15 01:04:26 -0600

    add seed script

--- server/database/data-seed.js
+++ server/database/data-seed.js
@@ -1,55 +1,57 @@
 const faker = require('faker');
-const mongoose = require('mongoose');
-const House = require('./House.js');
-const db = require('./index.js');
+// const mongoose = require('mongoose');
+// const House = require('./House.js');
+// const db = require('./index.js');
+const csv = require('fast-csv');
+const fs = require('fs');
 
 const random = num => Math.ceil(Math.random() * num);
 
-const zestHistory = () => {
-  let total = 300000;
-  const years = 8 + random(2);
-  const months = random(12);
-  let count = 0;
-  const spike = [12, 7, 12, 5, 8, 5, 14, 3, 19, 1000];
-  const slope = [
-    -4000,
-    -3000,
-    -1000,
-    2000,
-    5000,
-    2000,
-    5000,
-    3000,
-    10000,
-    7000,
-    700,
-    -700,
-  ];
-  let moreSlope = 0;
+// const zestHistory = () => {
+//   let total = 300000;
+//   const years = 8 + random(2);
+//   const months = random(12);
+//   let count = 0;
+//   const spike = [12, 7, 12, 5, 8, 5, 14, 3, 19, 1000];
+//   const slope = [
+//     -4000,
+//     -3000,
+//     -1000,
+//     2000,
+//     5000,
+//     2000,
+//     5000,
+//     3000,
+//     10000,
+//     7000,
+//     700,
+//     -700,
+//   ];
+//   let moreSlope = 0;
 
-  return Array.from({ length: years * 12 + months }, () => {
-    count++;
-    if (count % spike[0] === 0) {
-      const rand = random(4);
-      moreSlope = rand > 2 ? 2000 : rand === 2 ? -2000 : 0;
-      if (spike[0] === 14) {
-        moreSlope = 8000;
-      }
-      spike.shift();
-    }
-    total += slope[Math.floor(count / 12)] + moreSlope;
+//   return Array.from({ length: years * 12 + months }, () => {
+//     count++;
+//     if (count % spike[0] === 0) {
+//       const rand = random(4);
+//       moreSlope = rand > 2 ? 2000 : rand === 2 ? -2000 : 0;
+//       if (spike[0] === 14) {
+//         moreSlope = 8000;
+//       }
+//       spike.shift();
+//     }
+//     total += slope[Math.floor(count / 12)] + moreSlope;
 
-    return total + random(7000);
-  });
-};
+//     return total + random(7000);
+//   });
+// };
 
 const seedFunc = () => {
   let count = 0;
-  return Array.from({ length: 100 }, () => {
+  return Array.from({ length: 10000000 }, () => {
     const id = count;
     count++;
 
-    const zestimate = zestHistory();
+    const zestimate = [random(10), random(10), random(10), random(10), random(10)];
     return {
       _id: id.toString(),
       address: faker.address.streetAddress(),
@@ -66,12 +68,27 @@ const seedFunc = () => {
 };
 
 const seed = seedFunc();
-module.exports = seedFunc;
 
-const seedDatabase = () => {
-  House.create(seed)
-    .then(() => mongoose.connection.close())
-    .catch(err => console.error(err));
+const sdcSeed = () => {
+  console.time('time');
+  const csvStream = csv.createWriteStream({ headers: false });
+  const writableStream = fs.createWriteStream('my1.csv');
+  csvStream.pipe(writableStream);
+  for (let i = 0; i < 10000000; i++) {
+    csvStream.write(seed[i]);
+  }
+  csvStream.end();
+  console.timeEnd('time');
 };
 
-seedDatabase();
+sdcSeed();
+
+module.exports = seedFunc;
+
+// const seedDatabase = () => {
+//   House.create(seed)
+//     .then(() => mongoose.connection.close())
+//     .catch(err => console.error(err));
+// };
+
+// seedDatabase();
