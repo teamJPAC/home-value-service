128e052cee1632a7e761736904b7b8a2df06b09d
Jackie Chen <chen.jackie.k@gmail.com>
2018-11-15 01:03:54 -0600

    updated cassandra schema

--- server/database/cassandraPopulate.js
+++ server/database/cassandraPopulate.js
@@ -1,42 +1,42 @@
-var ExpressCassandra = require('express-cassandra');
+const ExpressCassandra = require('express-cassandra');
 
-var models = ExpressCassandra.createClient({
+const models = ExpressCassandra.createClient({
   clientOptions: {
     contactPoints: ['127.0.0.1'],
     protocolOptions: { port: 9042 },
     keyspace: 'mykeyspace',
-    queryOptions: { consistency: ExpressCassandra.consistencies.one }
+    queryOptions: { consistency: ExpressCassandra.consistencies.one },
   },
   ormOptions: {
     defaultReplicationStrategy: {
       class: 'SimpleStrategy',
-      replication_factor: 1
+      replication_factor: 1,
     },
     migration: 'safe',
-  }
+  },
 });
 
-var MyModel = models.loadSchema('Home', {
+const MyModel = models.loadSchema('Home', {
   fields: {
-    _id: "text",
-    address: "text",
-    city: "text",
-    zip: "int",
-    zestimate: { type: "set", typeDef: "<varint>"},
-    beds: "int",
-    baths: "int",
-    sqFt: "int",
-    status: "text",
-    taxAssessment: "int",
+    _id: 'text',
+    address: 'text',
+    city: 'text',
+    zip: 'int',
+    zestimate: { type: 'set', typeDef: '<varint>' },
+    beds: 'int',
+    baths: 'int',
+    sqFt: 'int',
+    status: 'text',
+    taxAssessment: 'int',
   },
-  key: ["_id"]
+  key: ['_id'],
 });
 
 console.log(models.modelInstance.Home === MyModel);
 
 MyModel.syncDB((err, result) => {
-  if(err){
+  if (err) {
     throw err;
   }
   console.log('schemed was altered', result);
-})
+});
