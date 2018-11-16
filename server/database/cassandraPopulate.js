3130a28ac11c879507bfce138b102f873feb3df5
Jackie Chen <chen.jackie.k@gmail.com>
2018-11-16 00:58:57 -0600

    modified cassandraPopulate

--- server/database/cassandraPopulate.js
+++ server/database/cassandraPopulate.js
@@ -4,7 +4,7 @@ const models = ExpressCassandra.createClient({
   clientOptions: {
     contactPoints: ['127.0.0.1'],
     protocolOptions: { port: 9042 },
-    keyspace: 'mykeyspace',
+    keyspace: 'house_info',
     queryOptions: { consistency: ExpressCassandra.consistencies.one },
   },
   ormOptions: {
@@ -18,18 +18,17 @@ const models = ExpressCassandra.createClient({
 
 const MyModel = models.loadSchema('Home', {
   fields: {
-    _id: 'text',
+    id: 'int',
     address: 'text',
     city: 'text',
     zip: 'int',
-    zestimate: { type: 'set', typeDef: '<varint>' },
-    beds: 'int',
-    baths: 'int',
-    sqFt: 'int',
+    beds: 'decimal',
+    baths: 'decimal',
+    sqft: 'decimal',
     status: 'text',
-    taxAssessment: 'int',
+    taxassessment: 'decimal',
   },
-  key: ['_id'],
+  key: ['id'],
 });
 
 console.log(models.modelInstance.Home === MyModel);
@@ -40,3 +39,34 @@ MyModel.syncDB((err, result) => {
   }
   console.log('schemed was altered', result);
 });
+
+
+// COPY house_info."Home"(id, address, city, zip, beds, baths, sqft, status, taxassessment) FROM '/Users/macbeth/Documents/Coding/hrr34/home-value-service/server/database/csv/test1.csv' WITH HEADER = FALSE;
+
+// COPY house_info."Home"(id, address, city, zip, beds, baths, sqft, status, taxassessment) FROM '/Users/macbeth/Documents/Coding/hrr34/home-value-service/server/database/csv/test2.csv' WITH HEADER = FALSE;
+
+// COPY house_info."Home"(id, address, city, zip, beds, baths, sqft, status, taxassessment) FROM '/Users/macbeth/Documents/Coding/hrr34/home-value-service/server/database/csv/test3.csv' WITH HEADER = FALSE;
+
+// COPY house_info."Home"(id, address, city, zip, beds, baths, sqft, status, taxassessment) FROM '/Users/macbeth/Documents/Coding/hrr34/home-value-service/server/database/csv/test4.csv' WITH HEADER = FALSE;
+
+// COPY house_info."Home"(id, address, city, zip, beds, baths, sqft, status, taxassessment) FROM '/Users/macbeth/Documents/Coding/hrr34/home-value-service/server/database/csv/test5.csv' WITH HEADER = FALSE;
+
+// COPY house_info."Home"(id, address, city, zip, beds, baths, sqft, status, taxassessment) FROM '/Users/macbeth/Documents/Coding/hrr34/home-value-service/server/database/csv/test6.csv' WITH HEADER = FALSE;
+
+// COPY house_info."Home"(id, address, city, zip, beds, baths, sqft, status, taxassessment) FROM '/Users/macbeth/Documents/Coding/hrr34/home-value-service/server/database/csv/test7.csv' WITH HEADER = FALSE;
+
+// COPY house_info."Home"(id, address, city, zip, beds, baths, sqft, status, taxassessment) FROM '/Users/macbeth/Documents/Coding/hrr34/home-value-service/server/database/csv/test8.csv' WITH HEADER = FALSE;
+
+// COPY house_info."Home"(id, address, city, zip, beds, baths, sqft, status, taxassessment) FROM '/Users/macbeth/Documents/Coding/hrr34/home-value-service/server/database/csv/test9.csv' WITH HEADER = FALSE;
+
+// COPY house_info."Home"(id, address, city, zip, beds, baths, sqft, status, taxassessment) FROM '/Users/macbeth/Documents/Coding/hrr34/home-value-service/server/database/csv/test10.csv' WITH HEADER = FALSE;
+
+// const url = '127.0.0.1: 9042';
+
+// const head_csv = 'head text.csv';
+
+// sbt “\
+// run - k house_info - t House - p c1, c2, c3, c4, c5, c6, c7, c8, c9 \
+// -c c1 = int, c2 = varchar, c3 = varchar, c4 = int, c5 = double, c6 = double, c7 = double, c8 = varchar, c9 = double \
+// -f / Users / macbeth / Documents / Coding / hrr34 / home - value - service / server / database / test.csv \
+// -o / Users / macbeth / Documents / Coding / hrr34 / home - value - service / server / database / house_info / House”
