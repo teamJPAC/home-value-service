ee8245f1a583be740aba6a2fe204ee2ce4105e7e
Jackie Chen <chen.jackie.k@gmail.com>
2018-11-16 23:56:26 -0600

    modified seed script to print in full csv

---
+++ server/database/seedCSV/seed1.js
@@ -0,0 +1,47 @@
+const faker = require('faker');
+const csv = require('fast-csv');
+const fs = require('fs');
+
+const random = num => Math.ceil(Math.random() * num);
+const randomRange = (min, max) => Math.floor(Math.random() * (max - min - 1) + min);
+
+let count = 1;
+
+const seedFunc = () => {
+  const id = count;
+  count++;
+
+  return {
+    _id: id.toString(),
+    address: faker.address.streetAddress(),
+    city: faker.address.city(),
+    zip: 98100 + random(99),
+    // zestimate,
+    beds: 3 + Math.floor(Math.random() * 2.5),
+    baths: 2.5 + 0.5 * Math.floor(Math.random() * 3),
+    sqFt: 1150 + 10 * random(20),
+    status: Math.random() < 0.5 ? 'For Sale' : 'Sold',
+    taxAssessment: randomRange(100000, 500000) * 0.937,
+  };
+};
+
+const sdcSeed = () => {
+  console.time('time');
+  const csvStream = csv.createWriteStream({ headers: false });
+  const writableStream = fs.createWriteStream(__dirname + '/csv/full.csv',{ flags: 'a' });
+  // const writableStream = fs.createWriteStream(__dirname + '/csv/full1.csv');
+
+  csvStream.on('finish', () => {
+    console.log('DONE!!!');
+  });
+  csvStream.pipe(writableStream);
+  for (let i = 1; i <= 1000000; i++) {
+    csvStream.write(seedFunc());
+  }
+  csvStream.end();
+  console.timeEnd('time');
+};
+
+sdcSeed();
+
+module.exports = seedFunc;
---
+++ server/database/seedCSV/seed3.js
@@ -0,0 +1,55 @@
+const faker = require('faker');
+const csv = require('fast-csv');
+const fs = require('fs');
+
+const random = num => Math.ceil(Math.random() * num);
+const randomRange = (min, max) => Math.floor(Math.random() * (max - min - 1) + min);
+
+let count = 2000000;
+
+const seedFunc = () => {
+  const id = count;
+  count++;
+
+  return {
+    _id: id.toString(),
+    address: faker.address.streetAddress(),
+    city: faker.address.city(),
+    zip: 98100 + random(99),
+    // zestimate,
+    beds: 3 + Math.floor(Math.random() * 2.5),
+    baths: 2.5 + 0.5 * Math.floor(Math.random() * 3),
+    sqFt: 1150 + 10 * random(20),
+    status: Math.random() < 0.5 ? 'For Sale' : 'Sold',
+    taxAssessment: randomRange(100000, 500000) * 0.937,
+  };
+};
+
+const sdcSeed = () => {
+  console.time('time');
+  const csvStream = csv.createWriteStream({ headers: false });
+  const writableStream = fs.createWriteStream(__dirname + '/csv/full.csv', { flags: 'a' });
+  // const writableStream = fs.createWriteStream(__dirname + '/csv/full3.csv');
+
+  csvStream.on('finish', () => {
+    console.log('DONE!!!');
+  });
+  csvStream.pipe(writableStream);
+  for (let i = 2000000; i <= 3000000; i++) {
+    csvStream.write(seedFunc());
+  }
+  csvStream.end();
+  console.timeEnd('time');
+};
+
+sdcSeed();
+
+module.exports = seedFunc;
+
+// const seedDatabase = () => {
+//   House.create(seed)
+//     .then(() => mongoose.connection.close())
+//     .catch(err => console.error(err));
+// };
+
+// seedDatabase();
