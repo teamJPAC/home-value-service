9a5dd681ccc034349253271ec4fc02db80dd9051
Jackie Chen <chen.jackie.k@gmail.com>
2018-11-26 23:02:33 -0600

    added more CRUD functionality

---
+++ server/postCRUD.js
@@ -0,0 +1,26 @@
+const { Pool } = require('pg');
+
+const connect = 'postgres://postgres:password@localhost:5432/housingdb';
+const db = new Pool({
+  connectionString: connect,
+  max: 20,
+});
+
+module.exports = {
+  create: () => {
+    console.log('Create');
+  },
+  read: async (...houseInfo) => {
+    const { rows } = await db.query(
+      `SELECT * FROM zillgo WHERE id in (${houseInfo})`,
+    );
+    console.log('ROWS --->', rows);
+    return rows;
+  },
+  update: () => {
+    console.log('Update');
+  },
+  delete: () => {
+    console.log('Delete');
+  },
+};
