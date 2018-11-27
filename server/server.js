aa97ff1d09b09bd7d110bcdb1feba8b6d787334b
Jackie Chen <chen.jackie.k@gmail.com>
2018-11-26 23:00:43 -0600

    made changes to server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('../newrelic.js');
+
 const mongoose = require('mongoose');
 const cors = require('cors');
 const express = require('express');
@@ -5,31 +7,32 @@ const graphqlHTTP = require('express-graphql');
 const path = require('path');
 const schema = require('./schema.js');
 
-const url = 'mongodb://localhost/houses';
+// const url = 'mongodb://localhost/houses';
+const url = 'postgres://localhost/housingdb';
 
 const port = 8081;
 
 const app = express();
 
-mongoose.Promise = global.Promise;
-mongoose.connect(
-  url,
-  { useNewUrlParser: true },
-);
-
-// app.use(cors());
-// app.use(express.static(`${__dirname}/../public`));
-
-// app.get('/:urlId', (req, res) => {
-//   res.sendFile(path.join(`${__dirname}/../public/index.html`));
-// });
-
-// app.post('/graphql',
-//   graphqlHTTP({
-//     schema,
-//     graphiql: false,
-//   }),
+// mongoose.Promise = global.Promise;
+// mongoose.connect(
+//   url,
+//   { useNewUrlParser: true },
 // );
+
+app.use(cors());
+app.use(express.static(`${__dirname}/../public`));
+
+app.get('/:urlId', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../public/index.html`));
+});
+
+app.post('/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: false,
+  }),
+);
 
 
 app.use(
